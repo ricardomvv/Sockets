7864dfffc325a04bfc2de27097ad5a204c3673bc
Ricardo Varela <ricardovarela@ufrn.edu.br>
2020-10-18 00:16:29 -0300

    Sockets TCP - Acesso remoto
    Sockets TCP - Acesso remoto

---
+++ SocketsTCP/RemotoClienteTCP.py
@@ -0,0 +1,12 @@
+# importacao das bibliotecas
+from socket import *
+
+# definicao das variaveis
+serverName = 'localhost' # ip do servidor
+serverPort = 61000 # porta a se conectar
+clientSocket = socket(AF_INET,SOCK_STREAM) # criacao do socket TCP
+clientSocket.connect((serverName, serverPort)) # conecta o socket ao servidor
+
+sentence = input('Digite um comando valido do Windows/Linux: ')
+clientSocket.send(sentence.encode('utf-8')) # envia o texto para o servidor
+clientSocket.close() # encerramento o socket do cliente
---
+++ SocketsTCP/RemotoServidorTCP.py
@@ -0,0 +1,24 @@
+# importacao das bibliotecas
+from socket import * # sockets
+import os
+
+# definicao das variaveis
+serverName = '' # ip do servidor (em branco)
+serverPort = 61000 # porta a se conectar
+serverSocket = socket(AF_INET,SOCK_STREAM) # criacao do socket TCP
+serverSocket.bind((serverName,serverPort)) # bind do ip do servidor com a porta
+serverSocket.listen(1) # socket pronto para 'ouvir' conexoes
+print ('Servidor TCP esperando conexoes na porta %d ...' % (serverPort))
+while 1:
+  connectionSocket, addr = serverSocket.accept() # aceita as conexoes dos clientes
+  comando = connectionSocket.recv(1024) # recebe dados do cliente
+  comando = comando.decode('utf-8')
+  #capitalizedSentence = sentence.upper() # converte em letras maiusculas
+  if os.system(comando) == 0:
+      print('Executado com sucesso')
+  else:
+      print('Erro ao executar o comando')
+  print ('Cliente %s enviou: %s' % (addr, comando))
+  #connectionSocket.send(capitalizedSentence.encode('utf-8')) # envia para o cliente o texto transformado
+  connectionSocket.close() # encerra o socket com o cliente
+serverSocket.close() # encerra o socket do servidor
