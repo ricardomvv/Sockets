70cf1d5c19b782856993a306202783f8f064b1ef
Ricardo Varela <ricardovarela@ufrn.edu.br>
2020-10-17 23:22:23 -0300

    Sockets UDP - Servidor de Data/Hora

---
+++ Servidor_UDP.py
@@ -0,0 +1,24 @@
+# importacao das bibliotecas
+from socket import * # sockets
+import time
+
+# definicao das variaveis
+serverName = '' # ip do servidor (em branco)
+serverPort = 61000 # porta a se conectar
+serverSocket = socket(AF_INET, SOCK_DGRAM) # criacao do socket UDP
+serverSocket.bind((serverName, serverPort)) # bind do ip do servidor com a porta
+print ('Servidor UDP esperando conexoes na porta %d ...' % (serverPort))
+while 1:
+    message, clientAddress = serverSocket.recvfrom(2048) # recebe do cliente
+    message = message.decode('utf-8')
+
+    if message == 'data':
+        modifiedMessage = str(time.ctime()) # converte para exibir a data e hora
+        print ('Cliente %s enviou: %s, transformando em: %s' % (clientAddress, message, modifiedMessage))
+        serverSocket.sendto(modifiedMessage.encode('utf-8'), clientAddress) 
+    else:
+        modifiedMessage = 'Comando invalido!'
+        print ('Cliente %s enviou: %s, transformando em: %s' % (clientAddress, message, modifiedMessage))
+        serverSocket.sendto(modifiedMessage.encode('utf-8'), clientAddress) 
+# envia a resposta para o cliente
+serverSocket.close() # encerra o socket do servidor
